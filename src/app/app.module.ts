c3b55b5e2a136623176ad68622602ab0cf42acf5
Mc Leonard Ledda <mcleonard13@gmail.com>
2024-08-20 23:07:17 +0800

    complete

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { Assignment6Component } from '@mcleonard13/assignment-6';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    Assignment6Component,
   ],
   providers: [],
   bootstrap: [AppComponent]
